b8d06d7989362ae79fc0aa69c92d34cca0b38b1a
Anthony Walz <32278324+AnthonyWalz@users.noreply.github.com>
2025-01-20 10:00:22 -0600

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,31 +1,21 @@
--- Delete all tables and indexes in public schema to reset for testing
+-- Delete all tables in public schema to reset for testing
 DO $$ 
 DECLARE 
     r RECORD;
 BEGIN
-    -- Drop all tables
     FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') 
     LOOP
         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-    END LOOP;
-
-    -- Drop all indexes except for users_pkey and users_email_key
-    FOR r IN (SELECT indexname FROM pg_indexes WHERE schemaname = 'public') 
-    LOOP
-        EXECUTE 'DROP INDEX IF EXISTS ' || quote_ident(r.indexname);
     END LOOP;
 END $$;
 
 -- Users table
 CREATE TABLE users (
-    id UUID PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone VARCHAR(20),
-    address TEXT,
+    address JSONB NOT NULL DEFAULT '[]'::JSONB,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
-
 
 -- Companies table
 CREATE TABLE companies (
@@ -34,7 +24,7 @@ CREATE TABLE companies (
     email VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR(20) UNIQUE NOT NULL,
     address TEXT UNIQUE NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
@@ -42,7 +32,8 @@ CREATE TABLE companies (
 CREATE TABLE product_variations (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE,
+    company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    product_variation_tags JSONB NOT NULL DEFAULT '[]'::JSONB,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
@@ -50,29 +41,42 @@ CREATE TABLE product_variations (
 CREATE TABLE products (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0), -- Ensure non-negative price
+    description TEXT NOT NULL CHECK (char_length(description) <= 1000),
+    price DECIMAL(10, 2) NOT NULL,
     image_urls TEXT[] NOT NULL,
     stock INT,
-    product_variation_id UUID REFERENCES product_variations(id) ON DELETE SET NULL,
-    company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE,
+    allow_negative_stock BOOLEAN NOT NULL DEFAULT FALSE,
+    product_variation_id UUID REFERENCES product_variations(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    product_variation_tags JSONB NOT NULL DEFAULT '[]'::JSONB,
+    company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 -- Carts table
 CREATE TABLE carts (
     id UUID PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    items JSONB NOT NULL DEFAULT '[]',
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    items JSONB NOT NULL DEFAULT '[]'::JSONB,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 -- Orders table
 CREATE TABLE orders (
     id UUID PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    items JSONB NOT NULL DEFAULT '[]',
-    total DECIMAL(10, 2) NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    items JSONB NOT NULL DEFAULT '[]'::JSONB,
+    total DECIMAL(10, 2) NOT NULL CHECK (total >= 0),
+    status VARCHAR(50) NOT NULL DEFAULT 'pending',
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+-- Sub Orders table (For handling multi-merchant orders)
+CREATE TABLE sub_orders (
+    id UUID PRIMARY KEY,
+    order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    items JSONB NOT NULL DEFAULT '[]'::JSONB,
+    total DECIMAL(10, 2) NOT NULL CHECK (total >= 0),
     status VARCHAR(50) NOT NULL DEFAULT 'pending',
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
